2d1b5f0e131e31fe48e7ee26c2be4db7cf7606c3
Jeszenői Bálint <balint.jeszenoi@griffsoft.hu>
2022-08-16 15:31:03 +0200

    init

---
+++ Catalog/Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,44 @@
+﻿using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Controllers
+{
+
+    // GET /items
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET /items react
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+        // GET /item/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
