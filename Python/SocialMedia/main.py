dbd46cba8db77b5e4b514e25a36844c6ce3cb81f
Jeszenői Bálint <balint.jeszenoi@griffsoft.hu>
2022-08-19 09:31:11 +0200

    Delete and proper status code responses

--- Python/SocialMedia/main.py
+++ Python/SocialMedia/main.py
@@ -1,9 +1,31 @@
-from fastapi import FastAPI
+from typing import Optional
+
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi import Body
+from pydantic import BaseModel
+from random import randrange
 
 #  create the instance
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts =[{"title": "Favourite food", "content": "Pizza", "id": 2}]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
+def find_post_index(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 # path operator or route
 # If we are at this url we do this methode
@@ -16,10 +38,33 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "yOUR POST"}
+    return {"data": my_posts}
 
 
-@app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"message": "Post created"}
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} does not exist")
+    return {"post": post}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 100000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+# title str, content str, category
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_post_index(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} doesn't exist")
+
+    my_posts.pop((index))
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
